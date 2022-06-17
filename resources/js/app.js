8933d04cca30927b6227cd4c6776f1e0072356e0
Walber Jefferson <walberjefferson@hotmail.com>
2022-06-16 22:52:37 -0300

    Ajuste página imovel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/js/bootstrap.js';
-// import * as bootstrap from 'bootstrap';
-// import {ScrollSpy} from 'bootstrap';
 require('./bootstrap');
+require('fslightbox');
 
 $(document).ready(function () {
     $(".favorito .fa").hover(function () {
@@ -9,48 +8,4 @@ $(document).ready(function () {
     }, function () {
         $(this).removeClass("fa-heart").addClass("fa-heart-o");
     });
-
-    // const dataSpyList = document.querySelectorAll('[data-bs-spy="scroll"]')
-    // dataSpyList.forEach(dataSpyEl => {
-    //     ScrollSpy.getInstance(dataSpyEl).refresh()
-    // })
 });
-
-
-// var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-//     target: '#listing-info-tabs-slider'
-// })
-
-// var exampleModal = document.getElementById('exampleModal')
-// exampleModal.addEventListener('show.bs.modal', function (event) {
-//     // Button that triggered the modal
-//     var button = event.relatedTarget
-//     // Extract info from data-bs-* attributes
-//     var recipient = button.getAttribute('data-bs-whatever')
-//     // If necessary, you could initiate an AJAX request here
-//     // and then do the updating in a callback.
-//     //
-//     // Update the modal's content.
-//     var modalTitle = exampleModal.querySelector('.modal-title')
-//     var modalBodyInput = exampleModal.querySelector('.modal-body input')
-//
-//     modalTitle.textContent = 'New message to ' + recipient
-//     modalBodyInput.value = recipient
-// })
-
-// var i = document.querySelectorAll("#listing-info-tabs-slider .listing-info-tabs a");
-// i.forEach((function (e, t) {
-//         e.addEventListener("click", (function (t) {
-//                 var a, s;
-//                 t.preventDefault(),
-//                     (null !== (a = document.querySelector("#listing-info-tabs-slider .listing-info-tabs a.active")) && void 0 !== a ? a : n.defaultElement).classList.remove("active"),
-//                     e.classList.add("active"),
-//                 e === i[i.length - 1] && r.setState({
-//                     stopScrollEvent: !0
-//                 });
-//                 var o = null !== (s = e.getAttribute("href")) && void 0 !== s ? s : "";
-//                 n.scrollTo("#tabs-navigation", o)
-//             }
-//         ))
-//     }
-// ));
--- webpack.mix.js
+++ webpack.mix.js
@@ -14,11 +14,14 @@ mix.js('resources/js/app.js', 'assets/js').sass('resources/sass/app.scss', 'asse
 //     ]
 // });
 
-mix.copy('node_modules/font-awesome-sass/assets/fonts/font-awesome/*', 'assets/fonts/font-awesome');
+mix.copy('node_modules/font-awesome-sass/assets/fonts/font-awesome/*', 'assets/fonts');
+mix.copy('node_modules/@fortawesome/fontawesome-free/webfonts/*', 'assets/webfonts');
+
 mix.copy('node_modules/font-awesome/css/font-awesome.min.css', 'assets/css');
+mix.copy('node_modules/@fortawesome/fontawesome-free/css/all.min.css', 'assets/css/font-awesome-6.min.css');
 
 mix.browserSync({
-    proxy: 'localhost:8080',
+    proxy: 'grupo-dragao.test',
     files: [
         '*.html',
         '**/*.(js|css)'
